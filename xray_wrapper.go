a12df535f4caf75688e79141793e6e9c13827060
Ggeshechka <ix.reylk@gmail.com>
2026-03-28 16:02:04 +0500

    .

--- xray_wrapper.go
+++ xray_wrapper.go
@@ -1,3 +1,4 @@
+// libXray is an Xray wrapper focusing on improving the experience of Xray-core mobile development.
 package libXray
 
 import (
@@ -8,6 +9,102 @@ import (
 	"github.com/xtls/libxray/nodep"
 	"github.com/xtls/libxray/xray"
 )
+
+type CountGeoDataRequest struct {
+	DatDir  string `json:"datDir,omitempty"`
+	Name    string `json:"name,omitempty"`
+	GeoType string `json:"geoType,omitempty"`
+}
+
+// Read geo data and write all codes to text file.
+func CountGeoData(base64Text string) string {
+	var response nodep.CallResponse[string]
+	req, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	var request CountGeoDataRequest
+	err = json.Unmarshal(req, &request)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	err = geo.CountGeoData(request.DatDir, request.Name, request.GeoType)
+	return response.EncodeToBase64("", err)
+}
+
+type readGeoFilesResponse struct {
+	Domain []string `json:"domain,omitempty"`
+	IP     []string `json:"ip,omitempty"`
+}
+
+// thin geo data
+func ReadGeoFiles(base64Text string) string {
+	var response nodep.CallResponse[*readGeoFilesResponse]
+	xray, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64(nil, err)
+	}
+	domain, ip := geo.ReadGeoFiles(xray)
+	var resp readGeoFilesResponse
+	resp.Domain = domain
+	resp.IP = ip
+	return response.EncodeToBase64(&resp, nil)
+}
+
+type pingRequest struct {
+	DatDir     string `json:"datDir,omitempty"`
+	ConfigPath string `json:"configPath,omitempty"`
+	Timeout    int    `json:"timeout,omitempty"`
+	Url        string `json:"url,omitempty"`
+	Proxy      string `json:"proxy,omitempty"`
+}
+
+// Ping Xray config and get the delay of its outbound.
+func Ping(base64Text string) string {
+	var response nodep.CallResponse[int64]
+	req, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64(nodep.PingDelayError, err)
+	}
+	var request pingRequest
+	err = json.Unmarshal(req, &request)
+	if err != nil {
+		return response.EncodeToBase64(nodep.PingDelayError, err)
+	}
+	delay, err := xray.Ping(request.DatDir, request.ConfigPath, request.Timeout, request.Url, request.Proxy)
+	return response.EncodeToBase64(delay, err)
+}
+
+// query inbound and outbound stats.
+func QueryStats(base64Text string) string {
+	var response nodep.CallResponse[string]
+	server, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+
+	stats, err := xray.QueryStats(string(server))
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	return response.EncodeToBase64(stats, nil)
+}
+
+// Test Xray Config.
+func TestXray(base64Text string) string {
+	var response nodep.CallResponse[string]
+	req, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	var request RunXrayRequest
+	err = json.Unmarshal(req, &request)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	err = xray.TestXray(request.DatDir, request.ConfigPath)
+	return response.EncodeToBase64("", err)
+}
 
 type RunXrayRequest struct {
 	DatDir       string `json:"datDir,omitempty"`
@@ -21,62 +118,100 @@ type RunXrayFromJSONRequest struct {
 	ConfigJSON   string `json:"configJSON,omitempty"`
 }
 
-type CountGeoDataRequest struct {
-	DatDir  string `json:"datDir,omitempty"`
-	Name    string `json:"name,omitempty"`
-	GeoType string `json:"geoType,omitempty"`
+// Create Xray Run Request
+func NewXrayRunRequest(datDir, mphCachePath, configPath string) (string, error) {
+	request := RunXrayRequest{
+		DatDir:       datDir,
+		MphCachePath: mphCachePath,
+		ConfigPath:   configPath,
+	}
+	requestBytes, err := json.Marshal(&request)
+	if err != nil {
+		return "", err
+	}
+
+	// Encode the JSON bytes to a base64 string
+	return base64.StdEncoding.EncodeToString(requestBytes), nil
 }
 
-// ВАЖНО: Пустая функция! Запрещаем Go закрывать FD.
-func manageFd(fd int) {}
+// Create Xray Run From JSON Request
+func NewXrayRunFromJSONRequest(datDir, mphCachePath, configJSON string) (string, error) {
+	request := RunXrayFromJSONRequest{
+		DatDir:       datDir,
+		MphCachePath: mphCachePath,
+		ConfigJSON:   configJSON,
+	}
+	requestBytes, err := json.Marshal(&request)
+	if err != nil {
+		return "", err
+	}
 
-func RunXray(fd int, base64Text string) string {
+	// Encode the JSON bytes to a base64 string
+	return base64.StdEncoding.EncodeToString(requestBytes), nil
+}
+
+// Run Xray instance.
+func RunXray(base64Text string) string {
 	var response nodep.CallResponse[string]
-	req, _ := base64.StdEncoding.DecodeString(base64Text)
+	req, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
 	var request RunXrayRequest
-	json.Unmarshal(req, &request)
-	err := xray.RunXray(request.DatDir, request.MphCachePath, request.ConfigPath)
+	err = json.Unmarshal(req, &request)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	err = xray.RunXray(request.DatDir, request.MphCachePath, request.ConfigPath)
 	return response.EncodeToBase64("", err)
 }
 
-func RunXrayFromJSON(fd int, base64Text string) string {
+// Run Xray instance with JSON configuration.
+func RunXrayFromJSON(base64Text string) string {
 	var response nodep.CallResponse[string]
-	req, _ := base64.StdEncoding.DecodeString(base64Text)
+	req, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
 	var request RunXrayFromJSONRequest
-	json.Unmarshal(req, &request)
-	err := xray.RunXrayFromJSON(request.DatDir, request.MphCachePath, request.ConfigJSON)
+	err = json.Unmarshal(req, &request)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	err = xray.RunXrayFromJSON(request.DatDir, request.MphCachePath, request.ConfigJSON)
 	return response.EncodeToBase64("", err)
 }
 
-func BuildMphCache(fd int, base64Text string) string {
-	var response nodep.CallResponse[string]
-	req, _ := base64.StdEncoding.DecodeString(base64Text)
-	var request RunXrayRequest
-	json.Unmarshal(req, &request)
-	err := xray.BuildMphCache(request.DatDir, request.MphCachePath, request.ConfigPath)
-	return response.EncodeToBase64("", err)
+// Get Xray State
+func GetXrayState() bool {
+	return xray.GetXrayState()
 }
 
+// Stop Xray instance.
 func StopXray() string {
 	var response nodep.CallResponse[string]
 	err := xray.StopXray()
 	return response.EncodeToBase64("", err)
 }
 
-func CountGeoData(base64Text string) string {
-	var response nodep.CallResponse[string]
-	req, _ := base64.StdEncoding.DecodeString(base64Text)
-	var request CountGeoDataRequest
-	json.Unmarshal(req, &request)
-	err := geo.CountGeoData(request.DatDir, request.Name, request.GeoType)
-	return response.EncodeToBase64("", err)
-}
-
-func GetXrayState() bool { return xray.GetXrayState() }
+// Xray's version
 func XrayVersion() string {
 	var response nodep.CallResponse[string]
 	return response.EncodeToBase64(xray.XrayVersion(), nil)
 }
-func TestXray(base64Text string) string { return "" }
-func QueryStats(server string) string   { return "" }
-func ReadGeoFiles(base64Text string) string { return "" }
+
+// Build Mph Cache
+func BuildMphCache(base64Text string) string {
+	var response nodep.CallResponse[string]
+	req, err := base64.StdEncoding.DecodeString(base64Text)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	var request RunXrayRequest
+	err = json.Unmarshal(req, &request)
+	if err != nil {
+		return response.EncodeToBase64("", err)
+	}
+	err = xray.BuildMphCache(request.DatDir, request.MphCachePath, request.ConfigPath)
+	return response.EncodeToBase64("", err)
+}
